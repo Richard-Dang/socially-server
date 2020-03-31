04b411bb32bda049e4d9a9db9eb0ccb42d0acb46
Richard Dang <richardspacedang@gmail.com>
2020-03-30 23:40:50 -0400

    Set up sign up and sign in endpoints

---
+++ src/index.js
@@ -0,0 +1,34 @@
+require("./models/User");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const requireAuth = require("./middleware/requireAuth");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@sociallycluster-flhko.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  userCreateIndex: true
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", err => {
+  console.error("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send("Homepage working!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
