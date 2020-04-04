68ad75deaf2363bd379f00fbf12ffe2d5cd31e87
Richard Dang <richardspacedang@gmail.com>
2020-04-04 17:33:36 -0400

    Add update social account endpoint

--- src/routes/socialAccountRoutes.js
+++ src/routes/socialAccountRoutes.js
@@ -8,15 +8,16 @@ const router = express.Router();
 
 router.use(requireAuth);
 
+// TODO: Change route to a get and accept params instead of a body by convention
+
 router.post("/socialaccounts", async (req, res) => {
   const { userId } = req.body;
-
   const socialAccounts = await SocialAccount.find({ userId });
 
   res.send(socialAccounts);
 });
 
-// TODO: Change route to a get and accept params instead of a body by convention
+// TODO: rename endpoint to /socialaccount
 router.post("/addsocialaccount", async (req, res) => {
   const { accountType, username } = req.body;
 
@@ -29,13 +30,24 @@ router.post("/addsocialaccount", async (req, res) => {
     const socialAccount = new SocialAccount({
       accountType,
       username,
-      userId: req.user._id
+      userId: req.user._id,
     });
     await socialAccount.save();
     res.send(socialAccount);
   } catch (err) {
     res.status(422).send({ error: err.message });
   }
+});
+
+router.put("/socialaccounts", async (req, res) => {
+  const socialAccount = req.body;
+  const filter = { _id: socialAccount._id };
+  const update = { username: socialAccount.username };
+
+  const updatedAccount = await SocialAccount.findOneAndUpdate(filter, update, {
+    new: true,
+  });
+  res.send(updatedAccount);
 });
 
 module.exports = router;
